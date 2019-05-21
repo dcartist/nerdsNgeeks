985a6eeda0ffa2ad4b8b049b19bf45b7836a603e
DCartist <pbannerman13@gmail.com>
2019-05-21 11:25:45 -0400

    working on setting up questions

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,19 @@ console.log("this is a test")
 console.log("another")
 console.log('abc')
 let questNumber = document.querySelector('#questNumber')
+let answer1 = document.querySelector('#answer1')
+let answer2 = document.querySelector('#answer2')
+let answer3 = document.querySelector('#answer3')
 questNumber.innerHTML = "This is another test"
+answer1.innerHTML = "testing #1"
+answer2.innerHTML = "testing #6"
+
+class Questions {
+    constructor(question, option1, option2, option3, realAnswer) {
+        this.question = question;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.realAnswer = realAnswer;
+    }
+}
